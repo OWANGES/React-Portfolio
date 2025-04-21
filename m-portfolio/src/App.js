354525396ae3168d7cb740983250765477462f16
OWANGE <omnikar11@gmail.com>
2025-04-21 18:15:35 -0400

    intial commit

---
+++ m-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // You can style it as you like
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Website</h1>
+        <p>This is a simple React website setup.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
